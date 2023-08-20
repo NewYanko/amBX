ac0ba52b52d254b93a2c73b7a11f625cb924adbb
yanko <113988004+NewYanko@users.noreply.github.com>
2023-08-20 15:16:43 +0100

    📂 Change structure Add media

---
+++ src/classes/AMBX.js
@@ -0,0 +1,44 @@
+import { Device } from "usb";
+import { Commands, Lights, USB } from "../Util.js";
+
+const ArrayLights = Object.keys(Lights).map((l) => Lights[l]);
+const RealArrayLights = ArrayLights.filter((l) => l != Lights.All);
+
+export default class AMBX {
+  /**
+   * USB Connection and Methods
+   * @param {Device} USBInstance
+   */
+  constructor(USBInstance) {
+    this.USB = USBInstance;
+    this.USB.open();
+    const inter = this.USB.interface(0);
+    inter.claim();
+    const end = inter.endpoint(USB.Endpoints.OUT);
+    this.Endpoint = end;
+  }
+
+  async Write(Light, Data = []) {
+    return new Promise((res) => {
+      this.Endpoint.makeTransfer(0, () => res()).submit(
+        Buffer.from([Commands.Header, Light, ...Data])
+      );
+    });
+  }
+
+  async SetColor(SelLights = Lights.All, Red, Green, Blue) {
+    if (!Array.isArray(SelLights))
+      if (ArrayLights.includes(SelLights))
+        SelLights = SelLights == Lights.All ? RealArrayLights : [SelLights];
+      else throw new Error("Invalid light");
+
+    for (const Light of SelLights) {
+      //console.log("Setting light", Light, "[", Red, Green, Blue, "]");
+      await this.Write(Light, [Commands.SetColor, Red, Green, Blue]);
+    }
+  }
+
+  Close() {
+    return this.USB.close();
+  }
+}
